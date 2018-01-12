78a89ffb1d0319f36a996e34b6e5e8f881d688ef
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-01-12 12:57:18 +0900

    update to v0.1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name='ppping',
-    version='0.1.3',
+    version='0.1.4',
     author='Mitsuo Heijo',
     author_email='mitsuo_h@outlook.com',
     description='Petty Plain Ping',
