08b88c7fa2609a8dc959026656540b449f37531e
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-01-12 13:05:39 +0900

    add config argument

--- ppping/script.py
+++ ppping/script.py
@@ -12,6 +12,7 @@ def set_args():
     parser.add_argument('--space', nargs='?', default=1, type=int, help='Space Size')
     parser.add_argument('-l', '--length', nargs='?', default=5, type=int, help='Result Length')
     parser.add_argument('-d', '--duration', nargs='?', default=sys.maxsize, type=int, help='Duration')
+    parser.add_argument('-c', '--config', type=open, help='Configuration file')
     parser.add_argument('-v', '--version', action='store_true', help='Version')
 
     return parser.parse_args()
@@ -28,7 +29,7 @@ def main():
         print('ppping v{}'.format(__VERSION__))
         return
 
-    p = PPPing(args.args, args.timeout, args.scale, args.length, args.space, args.duration)
+    p = PPPing(args.args, args.timeout, args.scale, args.length, args.space, args.duration, args.config)
 
     try:
         curses.wrapper(p.run)
