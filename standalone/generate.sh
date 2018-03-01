37ef2d1d7bb10ae43eac1d918517349dcf1b85f5
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-03-02 01:01:57 +0900

    add standalone script and fix README

---
+++ standalone/generate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+readonly PYTHON_COMMAND=python3
+readonly SCRIPT_DIR=$(cd $(dirname $0);pwd)
+readonly PARENT_DIR=$(cd $(dirname $0);cd ..;pwd)
+readonly PPPING_DIR=${PARENT_DIR}/ppping
+readonly GENERATED=${SCRIPT_DIR}/ppping
+
+echo "#!/usr/bin/env ${PYTHON_COMMAND}" > ${GENERATED}
+cat ${PPPING_DIR}/line.py ${PPPING_DIR}/parser.py ${PPPING_DIR}/ppping.py ${PPPING_DIR}/script.py ${PPPING_DIR}/__version__.py | grep -v "from" >> ${GENERATED}
+chmod +x ${GENERATED}
+echo -e "if __name__ == '__mainw__':\n    main()\n" >> ${GENERATED}
