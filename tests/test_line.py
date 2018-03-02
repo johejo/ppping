2e1918b3301d3bfbe7463adbee40a102e2698a68
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-03-02 22:44:13 +0900

    fix tests

--- tests/test_line.py
+++ tests/test_line.py
@@ -4,47 +4,47 @@ from ppping.parser import PingResult
 TXT_DIR = 'tests/txt/'
 
 
-class TestLine(object):
-    def test_line(self):
-        line = Line(0, arg='localhost', space=1)
+def test_line():
+    line = Line(0, arg='localhost', space=1)
 
-        with open(TXT_DIR + 'localhost.txt', 'rt') as f:
-            ping_message = f.read()
+    with open(TXT_DIR + 'localhost.txt', 'rt') as f:
+        ping_message = f.read()
 
-        line.add_info(PingResult(ping_message))
+    line.add_info(PingResult(ping_message))
+    line.add_char('▁')
+
+    assert line.x_pos() == 0
+    assert line.y_pos() == 1
+
+    expected = '>localhost  localhost ::1     0.06 ▁'
+    assert line.get_line('>', False, 10, 0, 9, 7, 4) == expected
+
+    for _ in range(5):
         line.add_char('▁')
 
-        assert line.x_pos() == 0
-        assert line.y_pos() == 1
+    line.reduce(result_len=5)
 
-        expected = '>localhost  localhost ::1     0.06 ▁'
-        assert line.get_line('>', False, 10, 0, 9, 7, 4) == expected
+    expected = '>localhost                 ::1     0.06 ▁▁▁▁▁'
+    assert line.get_line('>', True, 14, 10, 9, 7, 4) == expected
 
-        for _ in range(5):
-            line.add_char('▁')
 
-        line.reduce(result_len=5)
+def test_get_line():
 
-        expected = '>localhost                 ::1     0.06 ▁▁▁▁▁'
-        assert line.get_line('>', True, 14, 10, 9, 7, 4) == expected
+    with open(TXT_DIR + 'localhost.txt', 'rt') as f:
+        ping_message = f.read()
 
-    def test_get_line(self):
+    line = Line(0, arg='localhost', space=1)
 
-        with open(TXT_DIR + 'localhost.txt', 'rt') as f:
-            ping_message = f.read()
+    line.add_info(PingResult(ping_message))
+    line.add_char('▁')
 
-        line = Line(0, arg='localhost', space=1)
+    expected = '>localhost  ::1     0.06 ▁'
+    assert line.get_line('>', True, 10, 0, 9, 7, 4) == expected
 
-        line.add_info(PingResult(ping_message))
-        line.add_char('▁')
+    line = Line(0, arg='localhost', space=1)
 
-        expected = '>localhost  ::1     0.06 ▁'
-        assert line.get_line('>', True, 10, 0, 9, 7, 4) == expected
+    line.add_info(PingResult(ping_message))
+    line.add_char('▁')
 
-        line = Line(0, arg='localhost', space=1)
-
-        line.add_info(PingResult(ping_message))
-        line.add_char('▁')
-
-        expected = '>localhost             localhost ::1     0.06 ▁'
-        assert line.get_line('>', False, 10, 10, 9, 7, 4) == expected
+    expected = '>localhost             localhost ::1     0.06 ▁'
+    assert line.get_line('>', False, 10, 10, 9, 7, 4) == expected
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -15,46 +15,46 @@ class DummyResult(object):
         self.time = time
 
 
-class TestPingResult(object):
-    def test_parser(self):
+def test_parser():
 
-        expected = {
-            'localhost': DummyResult(address='::1', hostname='localhost',
-                                     icmp_seq=1, ttl=64, time=0.063),
-            '8.8.8.8': DummyResult(address='8.8.8.8', hostname='8.8.8.8',
-                                   icmp_seq=1, ttl=57, time=6.21),
-            'google.com': DummyResult(address='172.217.27.174',
-                                      hostname='kix05s07-in-f174.1e100.net',
-                                      icmp_seq=1, ttl=54, time=6.19),
-        }
+    expected = {
+        'localhost': DummyResult(address='::1', hostname='localhost',
+                                 icmp_seq=1, ttl=64, time=0.063),
+        '8.8.8.8': DummyResult(address='8.8.8.8', hostname='8.8.8.8',
+                               icmp_seq=1, ttl=57, time=6.21),
+        'google.com': DummyResult(address='172.217.27.174',
+                                  hostname='kix05s07-in-f174.1e100.net',
+                                  icmp_seq=1, ttl=54, time=6.19),
+    }
 
-        messages = [open(TXT_DIR + '{}.txt'.format(host), 'rt').read()
-                    for host in expected.keys()]
+    messages = [open(TXT_DIR + '{}.txt'.format(host), 'rt').read()
+                for host in expected.keys()]
 
-        message_raw = [m.encode() for m in messages]
+    message_raw = [m.encode() for m in messages]
 
-        for m, raw, e in zip(messages, message_raw, expected.values()):
-            result = PingResult(m)
-            assert result.raw == m
-            assert result.address == e.address
-            assert result.hostname == e.hostname
-            assert result.icmp_seq == e.icmp_seq
-            assert result.ttl == e.ttl
-            assert result.time == e.time
+    for m, raw, e in zip(messages, message_raw, expected.values()):
+        result = PingResult(m)
+        assert result.raw == m
+        assert result.address == e.address
+        assert result.hostname == e.hostname
+        assert result.icmp_seq == e.icmp_seq
+        assert result.ttl == e.ttl
+        assert result.time == e.time
 
-            result = PingResult(raw)
-            assert result.raw == m
-            assert result.address == e.address
-            assert result.hostname == e.hostname
-            assert result.icmp_seq == e.icmp_seq
-            assert result.ttl == e.ttl
-            assert result.time == e.time
+        result = PingResult(raw)
+        assert result.raw == m
+        assert result.address == e.address
+        assert result.hostname == e.hostname
+        assert result.icmp_seq == e.icmp_seq
+        assert result.ttl == e.ttl
+        assert result.time == e.time
 
-    def test_parser_error(self):
 
-        errors = [open(g, 'rt').read()
-                  for g in glob.glob(TXT_DIR + 'error*.txt')]
+def test_parser_error():
 
-        for e in errors:
-            with pytest.raises(PingParserError):
-                PingResult(e)
+    errors = [open(g, 'rt').read()
+              for g in glob.glob(TXT_DIR + 'error*.txt')]
+
+    for e in errors:
+        with pytest.raises(PingParserError):
+            PingResult(e)
