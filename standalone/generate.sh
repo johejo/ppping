367de8ffc9e7548cef476ccb577d1950cd36fab7
Mitsuo Heijo <mitsuo_h@outlook.com>
2018-03-02 21:13:17 +0900

    fix typo and standalone script generator

--- standalone/generate.sh
+++ standalone/generate.sh
@@ -6,7 +6,9 @@ readonly PARENT_DIR=$(cd $(dirname $0);cd ..;pwd)
 readonly PPPING_DIR=${PARENT_DIR}/ppping
 readonly GENERATED=${SCRIPT_DIR}/ppping
 
-echo "#!/usr/bin/env ${PYTHON_COMMAND}" > ${GENERATED}
-cat ${PPPING_DIR}/line.py ${PPPING_DIR}/parser.py ${PPPING_DIR}/ppping.py ${PPPING_DIR}/script.py ${PPPING_DIR}/__version__.py | grep -v "from" >> ${GENERATED}
+echo -e "#!/usr/bin/env ${PYTHON_COMMAND}\n\"\"\"" > ${GENERATED}
+cat ${PARENT_DIR}/LICENSE >> ${GENERATED}
+echo -e "\"\"\"\n" >> ${GENERATED}
+cat ${PPPING_DIR}/__version__.py ${PPPING_DIR}/line.py ${PPPING_DIR}/parser.py ${PPPING_DIR}/ppping.py ${PPPING_DIR}/script.py | grep -v "from" >> ${GENERATED}
 chmod +x ${GENERATED}
-echo -e "if __name__ == '__main__':\n    main()\n" >> ${GENERATED}
+echo -e "\n\nif __name__ == '__main__':\n    main()\n" >> ${GENERATED}
